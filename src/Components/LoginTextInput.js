e51b38579e1d3e887a0a3c9e17e09094724dd8a7
Brayan Hernandez <brayannhc07@gmail.com>
2022-03-24 10:42:35 -0600

    Se Agrega la Card para vacante y se corrige LoginTextInput

--- src/Components/LoginTextInput.js
+++ src/Components/LoginTextInput.js
@@ -1,21 +1,48 @@
 import React from 'react';
 
 const LoginTextInput = (props) => {
-	const { labelText = "", placeHolder = "", textValue, setTextValue, inputType = "text" } = props;
+	const { labelText = "", placeHolder = "", textValue, onChangeText, inputType = "text" } = props;
 
 	return (
-		<div className='login-text-input'>
-			<label >
-				<p>{labelText}</p>
+		<div style={styles.loginTextInput}>
+			<label>
+				<p style={styles.loginTextInputLabel}>{labelText}</p>
 				<input
+					style={styles.loginTextInputField}
 					placeholder={placeHolder}
 					value={textValue}
-					onChange={(e) => setTextValue(e.target.value)}
+					onChange={onChangeText}
 					type={inputType}
 				/>
 			</label>
 		</div>
 	);
 }
+
+
+const styles = {
+	loginTextInput: {
+		color: '#333',
+		margin: '4px'
+	},
+	loginTextInputLabel: {
+		fontSize: '12px',
+		margin: '2px 4px'
+	},
+	loginTextInputField: {
+		fontSize: '14px',
+		backgroundColor: '#eee',
+		border: '2px solid #999',
+		width: '100%',
+		borderRadius: '4px',
+		padding: '4px',
+
+		"&:focus": {
+			border: '2px solid #4b3eb8',
+			outline: 'none'
+		}
+	},
+
+};
 
 export default LoginTextInput;
