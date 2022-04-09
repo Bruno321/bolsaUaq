3fa88031ba9cb5b86c940d4b0dacf808aef88ac0
Alda53 <caldair99@gmail.com>
2022-04-08 21:03:54 -0500

    CSS y JS separados

--- src/Components/LoginTextInput.js
+++ src/Components/LoginTextInput.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './LoginTextInput.css'
 
 const LoginTextInput = (props) => {
 	const { labelText = "", placeHolder = "", textValue, onChangeText, inputType = "text" } = props;
 
 	return (
-		<div style={styles.loginTextInput}>
+		<div className='loginTextInput'>
 			<label>
-				<p style={styles.loginTextInputLabel}>{labelText}</p>
+				<p>{labelText}</p>
 				<input
-					style={styles.loginTextInputField}
 					placeholder={placeHolder}
 					value={textValue}
 					onChange={onChangeText}
@@ -18,31 +18,5 @@ const LoginTextInput = (props) => {
 		</div>
 	);
 }
-
-
-const styles = {
-	loginTextInput: {
-		color: '#333',
-		margin: '4px'
-	},
-	loginTextInputLabel: {
-		fontSize: '12px',
-		margin: '2px 4px'
-	},
-	loginTextInputField: {
-		fontSize: '14px',
-		backgroundColor: '#eee',
-		border: '2px solid #999',
-		width: '100%',
-		borderRadius: '4px',
-		padding: '4px',
-
-		"&:focus": {
-			border: '2px solid #4b3eb8',
-			outline: 'none'
-		}
-	},
-
-};
 
 export default LoginTextInput;
