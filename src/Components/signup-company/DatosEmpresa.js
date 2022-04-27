438c6dbc41a8942967f762c57acffecc62734451
Alda53 <caldair99@gmail.com>
2022-04-27 10:03:09 -0500

    Datos empresa
    Step 1 del stepform

--- src/Components/signup-company/DatosEmpresa.js
+++ src/Components/signup-company/DatosEmpresa.js
@@ -1,10 +1,22 @@
 import React from "react";
+//Estilos
+import './form.css';
 
-function DatosEmpresa (){
-    return(
-        <div className = "datos-empresa-container">
-        <input type = "text"/>
-    </div>
+function DatosEmpresa() {
+    return (
+        <div className="datos-empresa-container">
+            <a>Los campos con "*" son obligatorios</a>
+            <input type="text" placeholder="Nombre de la empresa*" />
+            <input type="text" placeholder="Usuario*" />
+            <input type="text" placeholder="Correo electrónico*" />
+            <input type="text" placeholder="Teléfono*" />
+            <button>Escoger logo*</button>
+            <input type = "text" placeholder="RFC de la empresa*"/>
+            <input type = "text" placeholder="Razón social*"/>
+            <input type = "text" placeholder="Sector*"/>
+            <input type = "text" placeholder="Descripción breve*"/>
+            <input type = "text" placeholder="Página web*"/>
+        </div>
     )
 }
 
