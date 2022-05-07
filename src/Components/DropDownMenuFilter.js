b49204f0058333efba7c9d0520249c273613e8e8
Helio Barriga <hbarriga16@alumnos.uaq.mx>
2022-05-06 23:43:12 -0500

    React - select y correción de estilos
    - Se corrigieron los estilos del componente DropDownMenuFilter.js
    - Se instaló la librería react - select para utilizarse en el componente DropDownMenuFilter.js

--- src/Components/DropDownMenuFilter.js
+++ src/Components/DropDownMenuFilter.js
@@ -1,74 +1,96 @@
 import React from 'react'
+import Select from "react-select";
+
+
+const options = [
+    { label: "Vacantes Aceptadas", value: "vacantesA"},
+    { label: "Vacantes Pendientes", value: "VacantesP"},
+    { label: "Vacantes Rechazadas", value: "VacantesR"}
+];
 
 const DropDownMenuFilter = () => {
     return (
         <div style={styles.styledSelect}>
-                <select style={styles.select}>
-                    <option style={styles.option} selected disabled>Seguimiento Vacantes</option>
-                    <option style={styles.option} value="vacantesA">Vacantes Aceptadas</option>
-                    <option style={styles.option} value="VacantesP">Vacantes Pendientes</option>
-                    <option style={styles.option} value="VacantesR">Vacantes Rechazadas</option>
-            </select>
+            <Select 
+                styles={customStyles} 
+                options={options}
+                placeholder="Seguimiento Vacantes"
+                isSearchable={false}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                      ...theme.colors,
+                      primary: 'none',
+                    },
+                  })}
+            />
         </div>
     )
 }
 
 
 const styles = {
-    header: {
-        fontFamily: 'Roboto',
-        fontSize: '300%',
-        marginTop: '5%',
-        marginLeft: '5%'
-    },
-    container: {
-        width: '80%',
-        height: '613px', //Estos margenes se quitan a la hora de implementar el componente/
-        marginLeft: '17%',
-        marginTop: '3.3%',
-        marginBottom: '3.5%',
-        border: '2px solid #000000'
-    },
     styledSelect: {
-        width: '18%',
+        width: '21%',
         height: '40px',
         marginLeft: '77%',
-        overflow: 'hidden',
         overflow: '-moz-hidden-unscrollable',
-        background: "url('https://vivesmart.com/wp-content/uploads/2019/03/white-down-arrow-png-2.png') no-repeat 98%/10% #005183",
         border: 'none',
         display: 'inline-block',
         position: 'relative',
-    },
-    select: {
-        background: 'transparent',
-        color: 'white',
-        width: '100%',
-        height: '100%',
-        webkitAppearance: 'none',
-        textAlign: 'center', 
+        textAlign: 'center',
         fontFamily: 'Roboto',
         fontSize: '170%',
-
-        "&:focus":{
-        borderColor: 'none',
-        boxShadow: 'none',
-        color: 'none', 
-        outline: 'none',
-        }
     },
-    option:{
-        color: 'rgb(0, 0, 0)',
-        backgroundColor:'#EEEEEE',
-        fontFamily: 'Roboto'
-    },
-    vacantes:{
-        width: '90%',
-        height: '454px', 
-        marginTop: '2%',
-        marginRight: '5%',
-        marginLeft: '5%',
-    }
 }
+
+const customStyles = {
+    container: (provided) => ({
+        ...provided,
+        marginTop: '-4%',
+        
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        marginTop: '-4.3%',
+        paddingBottom: '-.5%'
+    }),
+    placeholder: (provided) => ({
+        ...provided,
+        color: 'white'
+        
+    }),
+    singleValue: (provided) => ({
+        ...provided,
+        color: 'white'
+        
+    }),
+    control: (provided) => ({
+        ...provided,
+        backgroundColor: '#005183',
+        border: '1000px'
+    }),
+    indicatorsContainer: (provided) => ({
+        ...provided,
+        width: '15%',
+    }),
+    indicatorSeparator: (provided, state) => ({
+        ...provided,
+        display: 'none',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? '#ffffff' : '#000000',
+      backgroundColor: state.isSelected ? '#48456b' : '#EEEEEE',
+      padding: 20,
+      "&:hover":{
+        color: state.isSelected ? '#ffffff' : '#black',
+        backgroundColor: state.isSelected ? '#48456b' : '#cdcdcd'
+      },
+      textAlign: 'center',
+      fontFamily: 'Roboto'
+    }),
+  }
 
 export default DropDownMenuFilter
