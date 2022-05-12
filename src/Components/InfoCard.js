c274c0a81a9c3e635b06cdf0cf3323701ebb37af
Alda53 <caldair99@gmail.com>
2022-05-12 17:30:05 -0500

    estilos de infocard

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -1,11 +1,11 @@
-import React, {useContext} from "react";
-import {DetailContext} from '../Context/DetailContext'
-import {DataToShowContext} from '../Context/DataToShowContext'
+import React, { useContext } from "react";
+import { DetailContext } from '../Context/DetailContext'
+import { DataToShowContext } from '../Context/DataToShowContext'
 
-const InfoCard = ({props}) => {
+const InfoCard = ({ props }) => {
 
-    const {setData} = useContext(DetailContext)
-    const {optionSelected,setDetailSelected} = useContext(DataToShowContext)
+	const { setData } = useContext(DetailContext)
+	const { optionSelected, setDetailSelected } = useContext(DataToShowContext)
 
 	const onClickDetails = () => {
 	}
@@ -14,62 +14,66 @@ const InfoCard = ({props}) => {
 		setDetailSelected(true)
 	}
 	const elementsToRender = () => {
-		if(optionSelected==0){
+		if (optionSelected == 0) {
 			return (
 				<>
 					<p style={styles.vacantAttribute}> <b>Lugar de la empresa:</b>{props.ciudad}</p>
 					<p style={styles.vacantAttribute}> <b>RFC:</b>  {props.rfc}</p>
 					<p style={styles.vacantAttribute}> <b>Giro de la empresa:</b> {props.giro} </p>
-					<button style={styles.vacantDetails} onClick={()=>handleDetailButton()} className="btnHover">Mostrar detalles</button>
+					<button style={styles.vacantDetails} onClick={() => handleDetailButton()} className="btnHover">Mostrar detalles</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Aceptar</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Rechazar</button>
 				</>
 			)
 		}
-		if(optionSelected==1){
+		if (optionSelected == 1) {
 			return (
 				<>
 					<p style={styles.vacantAttribute}> <b>Horario:</b> {props.horario} </p>
 					<p style={styles.vacantAttribute}> <b>Salario:</b>  {props.rangoSueldo}</p>
 					<p style={styles.vacantAttribute}> <b>Nombre de la empresa:</b> {props.nombreEmpresa} </p>
-					<button style={styles.vacantDetails} onClick={()=>handleDetailButton()} className="btnHover">Mostrar detalles</button>
+					<button style={styles.vacantDetails} onClick={() => handleDetailButton()} className="btnHover">Mostrar detalles</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Aceptar</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Rechazar</button>
 				</>
 			)
 		}
-		if(optionSelected==2){
+		if (optionSelected == 2) {
 			return (
 				<>
 					<p style={styles.vacantAttribute}> <b>Horario:</b> {props.horario} </p>
 					<p style={styles.vacantAttribute}> <b>Salario:</b>  {props.rangoSueldo}</p>
 					<p style={styles.vacantAttribute}> <b>Nombre:</b> {props.nombreEmpresa} </p>
-					<button style={styles.vacantDetails} onClick={()=>handleDetailButton()} className="btnHover">Mostrar detalles</button>
+					<button style={styles.vacantDetails} onClick={() => handleDetailButton()} className="btnHover">Mostrar detalles</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Generar PDF</button>
 					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Ocupada/disponible</button>
 				</>
 			)
 		}
-		if(optionSelected==3){
+		if (optionSelected == 3) {
 			return (
 				<>
 					<p style={styles.vacantAttribute}> <b>Lugar:</b> {props.ciudad} </p>
 					<p style={styles.vacantAttribute}> <b>Rfc:</b>  {props.rfc}</p>
 					<p style={styles.vacantAttribute}> <b>Giro de la empresa:</b> {props.giro} </p>
-					<button style={styles.vacantDetails} onClick={()=>handleDetailButton()} className="btnHover">Mostrar detalles</button>
+					<button style={styles.vacantDetails} onClick={() => handleDetailButton()} className="btnHover">Mostrar detalles</button>
 				</>
 			)
 		}
-		if(optionSelected==4){
+		if (optionSelected == 4) {
 			return (
 				<>
-					<p style={styles.vacantAttribute}> <b>Horario:</b> {props.horario} </p>
-					<p style={styles.vacantAttribute}> <b>Salario:</b> {props.rangoSueldo} </p>
-					<p style={styles.vacantAttribute}> <b>Lugar:</b>  {props.ciudad}</p>
-					<p style={styles.vacantAttribute}> <b>Contacto:</b>  {props.contacto}</p>
+					<div style={{ display: "flex", justifyContent: "space-evenly" }}>
+						<p style={styles.vacantAttribute}> <b>Horario:</b> {props.horario} </p>
+						<p style={styles.vacantAttribute}> <b>Salario:</b> {props.rangoSueldo} </p>
+						<p style={styles.vacantAttribute}> <b>Lugar:</b>  {props.ciudad}</p>
+						<p style={styles.vacantAttribute}> <b>Contacto:</b>  {props.contacto}</p>
+					</div>
 					<p style={styles.vacantAttribute}> <b>Nombre de la empresa:</b> {props.nombreEmpresa} </p>
-					<button style={styles.vacantDetails} onClick={()=>handleDetailButton()} className="btnHover">Editar</button>
-					<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Disponible/llena</button>
+					<div style={{ display: "flex", justifyContent: "flex-end" }}>
+						<button style={styles.vacantDetails} onClick={() => handleDetailButton()} className="btnHover">Editar</button>
+						<button style={styles.vacantDetails} onClick={onClickDetails} className="btnHover">Disponible/Ocupada</button>
+					</div>
 				</>
 			)
 		}
@@ -93,7 +97,6 @@ const styles = {
 		padding: "4px 16px",
 		borderRadius: "4px",
 		margin: "4px",
-		display: "flex",
 		flexDirection: "column",
 		marginTop: "20px"
 
@@ -105,21 +108,20 @@ const styles = {
 		fontSize: "14px"
 	},
 	vacantAttribute: {
+		padding: "5px",
 		margin: "2px 0",
 		color: "#333",
-		fontSize: "11px"
+		fontSize: "15px"
 	},
 	vacantDetails: {
-
 		border: "none",
-		backgroundColor: "#226688",
+		backgroundColor: "#005183",
 		color: "#fff",
-		borderRadius: "4px",
-		padding: "6px 10px",
-		fontSize: "12px",
-		textAlign: "right",
-		margin: "4px",
-		marginLeft: "auto"
+		padding: "12px 25px",
+		fontSize: "15px",
+		margin: "10px",
+		position: "relative",
+		alignItems: "flex-end"
 	}
 }
 
